319f69d58749eb10a0630b07e747f455e468cba3
chiminhtathoj <vdlong2009@gmail.com>
2024-06-28 20:22:45 +0700

    Build UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,ts,css,scss,sass,less,styl}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-yellow': '#ffdb48',
+        'main-blue': '#3366cc',
+        'main-gray': '#f3f6f8',
+        'text-blur': '#75716c',
+        'btn-save': '#00c79a',
+        'btn-cancel': '#ff2020',
+        'main-text': '#d2e9f6'
+      },
+      fontFamily: {
+      },
+    },
   },
   plugins: [],
 }
